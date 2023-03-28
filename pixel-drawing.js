c6d225a253922576613d9940425b05a70582198c
romsn <romanbolek.ml@gmail.com>
2023-03-28 20:36:57 +0200

    fix

--- pixel-drawing.js
+++ pixel-drawing.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 const GRID_SIZE = 10; // Change this to change the size of the grid squares
-const colorPicker = document.querySelector('#color-picker');
-const saveButton = document.querySelector('#save-button');
+const colorPicker = document.querySelector('#colorPicker');
+const saveButton = document.querySelector('#saveButton');
 
 let isDrawing = false;
 
@@ -37,7 +37,7 @@ saveButton.addEventListener('click', savePicture);
 function savePicture() {
 	const dataUrl = canvas.toDataURL(); // Get a data URL representing the contents of the canvas
 	const link = document.createElement('a');
-	link.download = 'picture.png';
+	link.download = 'pixl.png';
 	link.href = dataUrl;
 	document.body.appendChild(link);
 	link.click();
