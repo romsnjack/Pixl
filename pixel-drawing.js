9fe441a8b1352d8190d8fce2effb358499457e75
romsn <romanbolek.ml@gmail.com>
2023-03-29 09:19:57 +0200

    Fill the color

--- pixel-drawing.js
+++ pixel-drawing.js
@@ -3,16 +3,24 @@ const ctx = canvas.getContext('2d');
 const GRID_SIZE = 10; // Change this to change the size of the grid squares
 const colorPicker = document.querySelector('#colorPicker');
 const saveButton = document.querySelector('#saveButton');
+const eraseButton = document.querySelector('#eraseButton');
+//const fillButton = document.querySelector('#fillButton');
 
 let isDrawing = false;
+let isErasing = false;
 
 canvas.addEventListener('mousedown', startDrawing);
 canvas.addEventListener('mouseup', stopDrawing);
-canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousemove', draw, erase);
 
 function startDrawing(e) {
-	isDrawing = true;
-	draw(e);
+	if (isErasing) {
+		isDrawing = false;
+		erase(e);
+	} else {
+		isDrawing = true;
+		draw(e);
+	}
 }
 
 function stopDrawing() {
@@ -32,6 +40,18 @@ function draw(e) {
 	ctx.fillRect(col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE);
 }
 
+function erase(e) {
+	if (!isErasing) return;
+
+	const x = e.offsetX;
+	const y = e.offsetY;
+
+	const row = Math.floor(y / GRID_SIZE);
+	const col = Math.floor(x / GRID_SIZE);
+
+	ctx.clearRect(col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+}
+
 saveButton.addEventListener('click', savePicture);
 
 function savePicture() {
@@ -42,4 +62,15 @@ function savePicture() {
 	document.body.appendChild(link);
 	link.click();
 	document.body.removeChild(link);
+}
+
+eraseButton.addEventListener('click', toggleErase);
+
+function toggleErase() {
+	isErasing = !isErasing;
+	if (isErasing) {
+		eraseButton.classList.add('active');
+	} else {
+		eraseButton.classList.remove('active');
+	}
 }
