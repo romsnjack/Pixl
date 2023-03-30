1b7f4f0f258ad664d5714183f1805f4817d0cba5
romsn <romanbolek.ml@gmail.com>
2023-03-30 20:55:07 +0200

    Add change size

--- pixel-drawing.js
+++ pixel-drawing.js
@@ -1,10 +1,12 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
-const GRID_SIZE = 10; // Change this to change the size of the grid squares
 const colorPicker = document.querySelector('#color-picker');
 const saveButton = document.querySelector('#save-button');
 const clearButton = document.querySelector('#clear-button');
+const sizeRange = document.getElementById('size-range');
+const sizeValue = document.getElementById('size-value');
 
+let pixelSize = 10;
 let isDrawing = false;
 let isErasing = false;
 
@@ -12,42 +14,43 @@ canvas.addEventListener('mousedown', startDrawing);
 canvas.addEventListener('mouseup', stopDrawing);
 canvas.addEventListener('mousemove', draw);
 
-function startDrawing(e) {
-	if (isErasing) {
-		isDrawing = false;
-		erase(e);
-	} else {
-		isDrawing = true;
-		draw(e);
-	}
+function startDrawing() {
+	isDrawing = true;
 }
 
 function stopDrawing() {
 	isDrawing = false;
 }
 
+sizeRange.addEventListener('input', function () {
+	// Update pixel size
+	pixelSize = parseInt(this.value);
+	sizeValue.textContent = sizeRange.value;
+});
+
 function draw(e) {
 	if (!isDrawing) return;
 
-	const x = e.offsetX;
-	const y = e.offsetY;
+	const rect = canvas.getBoundingClientRect();
+	const x = e.clientX - rect.left - pixelSize;
+	const y = e.clientY - rect.top - pixelSize;
 
 	if (e.buttons === 1) {
 		// left mouse button is being held down
 		ctx.fillStyle = colorPicker.value;
 		ctx.fillRect(
-			Math.floor(x / GRID_SIZE) * GRID_SIZE,
-			Math.floor(y / GRID_SIZE) * GRID_SIZE,
-			GRID_SIZE,
-			GRID_SIZE
+			Math.floor(x / pixelSize) * pixelSize,
+			Math.floor(y / pixelSize) * pixelSize,
+			pixelSize,
+			pixelSize
 		);
 	} else if (e.buttons === 2) {
 		// right mouse button is being held down
 		ctx.clearRect(
-			Math.floor(x / GRID_SIZE) * GRID_SIZE,
-			Math.floor(y / GRID_SIZE) * GRID_SIZE,
-			GRID_SIZE,
-			GRID_SIZE
+			Math.floor(x / pixelSize) * pixelSize,
+			Math.floor(y / pixelSize) * pixelSize,
+			pixelSize,
+			pixelSize
 		);
 	}
 }
